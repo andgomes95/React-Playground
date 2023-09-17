22c26aebadcf119df3c4ea7a272e924a1ef48245
Andre Gomes <andgomes95@gmail.com>
2023-09-17 12:35:02 -0300

    Prototype of pagination works!

--- src/components/AnimeSeason/index.tsx
+++ src/components/AnimeSeason/index.tsx
@@ -1,11 +1,10 @@
 import { useEffect, useState } from "react";
 import { CardContainer } from "../DefaultCard/styles";
-import SearchInput from "../SearchInput";
 import AnimeSeasonCard from "./AnimeSeasonCard";
 import axios from 'axios';
-import { Themes } from "./utils";
+import { Pagination, Themes } from "./utils";
 
-interface Anime {
+export interface Anime {
   "title": string,
   "images": any,
   "synopsis": string,
@@ -23,35 +22,45 @@ interface Anime {
 
 
 function AnimeSeason() {
-  const [animes, setaAnimes] = useState<Anime[]>([]);
   const [searchAnime, setSearchAnime] = useState<Anime[]>([]);
-   const [pagination, setPagination] = useState(0);
+   const [pagination, setPagination] = useState<Pagination>(
+    {
+      "last_visible_page": 1,
+      "has_next_page": false,
+      "current_page": 1
+  });
+  const [currentPage, setCurrentPage] = useState(pagination.current_page);
+
+  const handlePageChange = (newPage: number) => {
+    setCurrentPage(newPage);
+    getEspecificPage(currentPage);
+  };
+
+  async function getEspecificPage(page: number){
+    try {
+      const animesAux = await axios.get(`https://api.jikan.moe/v4/seasons/upcoming?page=${page}`);
+      setSearchAnime(animesAux.data.data);
+      setPagination(animesAux.data.pagination);
+    } catch (error) {
+      console.error('Erro ao buscar os posts:', error);
+    }
+  }
 
   useEffect(() => {
     async function fetchGet() {
       try {
         const animesAux = await axios.get('https://api.jikan.moe/v4/seasons/upcoming');
-        setaAnimes(animesAux.data.data);
         setSearchAnime(animesAux.data.data);
-        setPagination(animesAux.data.pagination.last_visible_page);
+        setPagination(animesAux.data.pagination);
       } catch (error) {
         console.error('Erro ao buscar os posts:', error);
       }
     }
     fetchGet();
-  }, [])
+  }, []);
 
   return (
     <CardContainer>
-      <SearchInput
-        type="text"
-        placeholder="Busque um anime"
-        onBlur={(e) => {
-          const typeText = e.target.value;
-          const result = animes.filter((anime) => anime.title.includes(typeText))
-          setSearchAnime(result);
-        }}
-      />
       {searchAnime.map((anime) => {
         let themes = [...anime.themes, ...anime.genres,...anime.demographics ];
         return (<>
@@ -66,6 +75,23 @@ function AnimeSeason() {
           />
           </>)
       })}
+      <div>
+        <h1>Paginação de Itens</h1>
+        <ul>
+          {searchAnime.map((item, index) => (
+            <li key={index}>{item.title}</li>
+          ))}
+        </ul>
+        <nav>
+          <ul className="pagination">
+            {Array.from({ length: pagination.last_visible_page}, (_, index) => (
+                <button className="page-link" onClick={() => handlePageChange(index + 1)}>
+                  {index + 1}
+                </button>
+            ))}
+          </ul>
+        </nav>
+      </div>
     </CardContainer>
   );
 }
--- src/components/AnimeSeason/utils.ts
+++ src/components/AnimeSeason/utils.ts
@@ -1,6 +1,12 @@
 
 export type Themes = {
-  "mal_id": 1,
-  "type": "anime",
-  "name": "Action"
+  "mal_id": number,
+  "type": string,
+  "name": string
+}
+
+export type Pagination = {
+  "last_visible_page": number,
+  "has_next_page": boolean,
+  "current_page": number
 }
